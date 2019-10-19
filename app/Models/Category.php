d607691107b28e60141101502e65b80b5e20fcf3
ma7euus <mateuspastori@outlook.com>
2019-10-19 14:34:19 -0300

    category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
-{
-    //
+class Category extends Model {
+    protected $fillable = [
+        'name', 'description', 'is_active'
+    ];
 }

a4fafd6a300ba687194ce48ec0c8b41999321d34
ma7euus <mateuspastori@outlook.com>
2019-10-21 23:19:57 -0300

    category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -33,6 +33,7 @@ class CategoryController extends Controller {
     }
 
     public function destroy(Category $category) {
-        //
+        $category->delete();
+        return response()->noContent();
     }
 }

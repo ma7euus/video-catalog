892e9ceffab605ea0199d465151b60a069fc3f7a
Mateus <mateuspastori@outlook.com>
2019-10-30 13:09:49 -0300

    test categories

--- tests/Unit/CategoryTest.php
+++ tests/Unit/CategoryTest.php
@@ -3,18 +3,45 @@
 namespace Tests\Unit;
 
 use App\Models\Category;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CategoryTest extends TestCase {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testExample() {
+
+    public function testIfUseTraits() {
+        $traits = [
+            SoftDeletes::class, Uuid::class
+        ];
+        $categoryTraits = array_keys(class_uses(Category::class));
+        $this->assertEquals($traits, $categoryTraits);
+    }
+
+    public function testFillableAttribute() {
+        $fillable = ['name', 'description', 'is_active'];
         $category = new Category();
-        $this->assertEquals(['name', 'description', 'is_active'], $category->getFillable());
+        $this->assertEquals($fillable, $category->getFillable());
+    }
+
+    public function testCastsAttribute() {
+        $casts = ['id' => 'string'];
+        $category = new Category();
+        $this->assertEquals($casts, $category->getCasts());
+    }
+
+    public function testDatesAttribute() {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        $category = new Category();
+        foreach ($dates as $date) {
+            $this->assertContains($date, $category->getDates());
+        }
+        $this->assertCount(count($dates), $category->getDates());
+    }
+
+    public function testIncrementingAttribute() {
+        $category = new Category();
+        $this->assertEquals(false, $category->getIncrementing());
     }
 }

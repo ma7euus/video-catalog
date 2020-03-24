c9102089038103336146ae4bf842c7c475e1fef1
ma7euus <mateuspastori@outlook.com>
2020-03-24 00:48:33 -0300

    react

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
+
+## FRONT-END
+npm config set cache /var/www/.npm-cache --global
+cd /var/www/frontend && npm install && cd ..
+
+## BACK-END
 cd backend
 if [ ! -f ".env" ]; then
   cp .env.example .env
+fi
 if [ ! -f ".env.testing" ]; then
   cp .env.testing.example .env.testing
+fi
 
 composer install
 php artisan key:generate
@@ -15,7 +23,9 @@ php artisan ide-helper:model > /dev/null << EOF
 <no>
 EOF
 
-php artisan storage:link
+if [ ! -L "/var/www/backend/public/storage" ]; then
+  php /var/www/backend/artisan storage:link
+fi
 
 chmod -R 777 ./
 

721b4bb5cffdf900c2906c30a4d5afc0854da968
ma7euus <mateuspastori@outlook.com>
2020-03-30 22:53:22 -0300

    frontend navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,12 +1,27 @@
 // @flow
 import * as React from 'react';
-import {AppBar, Toolbar} from "@material-ui/core";
+import {AppBar, makeStyles, Button, Toolbar, Typography} from "@material-ui/core";
+import logo from '../../static/img/logo.png';
+
+const useStyles = makeStyles({
+    toolbar: {
+        backgroundColor: '#000000',
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'center'
+    }
+});
 
 export const Navbar: React.FC = () => {
+    const classes = useStyles();
     return (
         <AppBar>
-            <Toolbar>
-                
+            <Toolbar className={classes.toolbar}>
+                <Typography className={classes.title}>
+                    <img src={logo} alt="CodeFlix"/>
+                </Typography>
+                <Button color="inherit">Login</Button>
             </Toolbar>
         </AppBar>
     );

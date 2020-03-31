7239775495d49214fb920caabd934bbc93e82582
ma7euus <mateuspastori@outlook.com>
2020-03-30 22:29:06 -0300

    frontend navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+// @flow
+import * as React from 'react';
+import {AppBar, Toolbar} from "@material-ui/core";
+
+export const Navbar: React.FC = () => {
+    return (
+        <AppBar>
+            <Toolbar>
+                
+            </Toolbar>
+        </AppBar>
+    );
+};

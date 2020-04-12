3ab68918f91f991b70314972d25d9f863bdda07b
ma7euus <mateuspastori@outlook.com>
2020-04-11 21:35:44 -0300

    validate, snack - crud category

--- frontend/src/pages/category/Form.tsx
+++ frontend/src/pages/category/Form.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
-import {Box, Button, ButtonProps, Checkbox, TextField, Theme} from "@material-ui/core";
+import {Box, Button, ButtonProps, Checkbox, FormControlLabel, TextField, Theme} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import {useForm} from "react-hook-form";
 import categoryHttp from "../../util/http/category-http";
+import * as Yup from '../../util/vendor/yup';
+import {useEffect, useState} from "react";
+import {useParams, useHistory} from "react-router";
+import {useSnackbar} from "notistack";
 
 const useStyles = makeStyles((theme: Theme) => {
     return {
@@ -12,25 +16,91 @@ const useStyles = makeStyles((theme: Theme) => {
     }
 });
 
-export const Form = () => {
+const validationSchema = Yup.object().shape({
+    name: Yup.string()
+        .label('Nome')
+        .max(255)
+        .required(),
+});
+
+export const Form: React.FC = () => {
     const classes = useStyles();
+
+    const {
+        register,
+        handleSubmit,
+        getValues,
+        setValue,
+        errors,
+        reset,
+        watch
+    } = useForm({
+        validationSchema,
+        defaultValues: {
+            name: '',
+            is_active: true
+        }
+    });
+
+    const snackbar = useSnackbar();
+    const history = useHistory();
+    const {id} = useParams();
+    const [category, setCategory] = useState<{ id: string } | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
     const buttonProps: ButtonProps = {
         className: classes.submit,
         color: "secondary",
         variant: "contained",
+        disabled: loading,
     };
 
-    const {register, handleSubmit, getValues} = useForm({
-        defaultValues: {
-            is_active: true
-        }
-    });
+    useEffect(() => {
+        register({name: "is_active"})
+    }, [register]);
 
-    function onSubmit(formData) {
+    useEffect(() => {
+        if (!id) {
+            return;
+        }
+        setLoading(true);
         categoryHttp
-            .create(formData)
-            .then((response) => console.log(response));
+            .get(id)
+            .then(({data}) => {
+                setCategory(data.data);
+                reset(data.data);
+            })
+            .finally(() => setLoading(false));
+    }, [id, reset]);
+
+    function onSubmit(formData, event) {
+        setLoading(true);
+        const http = !category
+            ? categoryHttp.create(formData)
+            : categoryHttp.update(category.id, formData);
+        http.then(
+            (response) => {
+                snackbar.enqueueSnackbar(
+                    "Categoria salva com sucesso!",
+                    {variant: 'success'}
+                );
+                setTimeout(() => {
+                    event
+                        ? id
+                        ? history.replace(`/categories/${response.data.data.id}/edit`)
+                        : history.push(`/categories/${response.data.data.id}/edit`)
+                        : history.push('/categories');
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+                snackbar.enqueueSnackbar(
+                    "Não foi possível salvar a categoria",
+                    {variant: 'error'}
+                );
+            })
+            .finally(() => setLoading(false));
+        ;
     }
 
     return (
@@ -41,6 +111,10 @@ export const Form = () => {
                 fullWidth
                 variant={"outlined"}
                 inputRef={register}
+                disabled={loading}
+                error={errors.name !== undefined}
+                helperText={errors.name && errors.name.message}
+                InputLabelProps={{shrink: true}}
             />
             <TextField
                 name="description"
@@ -51,16 +125,26 @@ export const Form = () => {
                 variant={"outlined"}
                 margin={"normal"}
                 inputRef={register}
+                disabled={loading}
+                InputLabelProps={{shrink: true}}
             />
-            <Checkbox
-                name="is_active"
-                inputRef={register}
-                defaultChecked
-                color={"primary"}
+            <FormControlLabel
+                disabled={loading}
+                control={
+                    <Checkbox
+                        name="is_active"
+                        color={"primary"}
+                        onChange={
+                            () => setValue('is_active', !getValues()['is_active'])
+                        }
+                        checked={watch('is_active')}
+                    />
+                }
+                label={'Ativo?'}
+                labelPlacement={'end'}
             />
-            Ativo?
             <Box dir={"rtl"}>
-                <Button {...buttonProps} onClick={() => onSubmit(getValues())}>Salvar</Button>
+                <Button {...buttonProps} onClick={() => onSubmit(getValues(), null)}>Salvar</Button>
                 <Button {...buttonProps} type="submit">Salvar e continuar editando</Button>
             </Box>
         </form>
--- frontend/src/pages/category/PageForm.tsx
+++ frontend/src/pages/category/PageForm.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
+import {useParams} from 'react-router';
 import {Page} from "../../components/Page";
 import {Form} from "./Form";
 
-const PageForm = () => {
+const PageForm: React.FC = () => {
+    const {id} = useParams();
+
     return (
-        <Page title={'Cadastrar Categoria'}>
+        <Page title={!id ? 'Criar categoria' : 'Editar categoria'}>
             <Form/>
         </Page>
-
     );
 };
 

2603bfc284a36de6da4f0a851aff54797cf7389c
ma7euus <mateuspastori@outlook.com>
2020-06-01 23:35:09 -0300

    video (crud)

--- backend/app/ModelFilters/GenreFilter.php
+++ backend/app/ModelFilters/GenreFilter.php
@@ -8,7 +8,7 @@ class GenreFilter extends DefaultModelFilter {
     protected $sortable = ['name', 'created_at'];
 
     public function search($search) {
-        $this->where('name', 'LIKE', "%$search%");
+        $this->where('name', 'LIKE', "%$search%")->with('categories');
     }
 
     public function isActive($isActive) {
